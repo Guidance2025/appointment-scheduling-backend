cb2c072e8ca80f0865dcdb45c7248c98bd233783
NNeonael <neonaelangcayanazareno@gmail.com>
2025-11-21 00:50:55 +0800

    fix failed build

--- src/main/java/org/rocs/asa/AppointmentSchedulingBackendApplication.java
+++ src/main/java/org/rocs/asa/AppointmentSchedulingBackendApplication.java
@@ -2,9 +2,7 @@ package org.rocs.asa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
