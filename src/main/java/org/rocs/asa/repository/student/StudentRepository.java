df0aa43e4a511862129eceac8ffa6c05aa5f95a1
NNeonael <neonaelangcayanazareno@gmail.com>
2026-02-01 14:10:19 +0800

    added new feature

--- src/main/java/org/rocs/asa/repository/student/StudentRepository.java
+++ src/main/java/org/rocs/asa/repository/student/StudentRepository.java
@@ -5,6 +5,8 @@ import org.rocs.asa.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
@@ -12,6 +14,8 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     Student findStudentByStudentNumber(String studentNumber);
     boolean existsByStudentNumber(String StudentNumber);
     Student findByUser (User user);
+    List<Student> findTop10ByStudentNumberStartingWithIgnoreCase(String studentNumber);
+
 //    List<StudentListItemDto> listStudentsWithHasResponse(String course, String cluster);
 //    Optional<StudentInfoDto> findStudentInfo(Long studentId);
 }
--- src/main/java/org/rocs/asa/service/student/StudentService.java
+++ src/main/java/org/rocs/asa/service/student/StudentService.java
@@ -2,8 +2,12 @@ package org.rocs.asa.service.student;
 
 import org.rocs.asa.domain.student.Student;
 import org.rocs.asa.domain.user.User;
+
+import java.util.List;
+
 public interface StudentService {
 
     Student findByAuthenticatedStudent();
     Student findByUser(User user);
+    List<Student> findBtStudentNumberStartingWith(String studentNumber);
 }
