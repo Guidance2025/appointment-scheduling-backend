f6ab4bc233e6fc8357a03c78de5534a6aa3f9b18
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-02-05 02:16:37 +0800

    added : update functionality for admin (#40)
    * Create Student Profile / Added Some Docu
    
    * fix appointment service
    
    * fix build fail
    
    * fix build failed
    
    * testing
    
    * update
    
    * Add Firebase Service Account,json to .gitignore
    
    * added : functionalities
    
    * fix failed build
    
    * Implement : Clear Notification / Admin Initializer
    
    * new features added
    
    * latest update
    
    * latest
    
    * updated
    
    * clean
    
    * refactor/add: self assessment , clean up some services
    
    * updated new feature added
    
    * update
    
    * Dockerized backend and update service
    
    * add docker ci check
    
    * fix build fail
    
    * fix :create post feature / retrieving student section
    
    * add
    
    * fix bugs added new feature
    
    * fix
    
    * fix added sql initialize
    
    * fix
    
    * fixing post getAllPostBySectionID
    
    * update
    
    * update
    
    * added new feature
    
    * Deploy Backend
    
    * fix conflict
    
    * fix
    
    * fix bug
    
    * fix bug
    
    * Add docker-compose.yml and fix .gitignore
    
    * fix posgres migration / cloud deployement
    
    * fix build
    
    * fix deployment bug
    
    * fix dep bug
    
    * fix
    
    * fix post feature
    
    * updated
    
    * fix deployment bug
    
    * added : update functionality for admin
    
    ---------
    
    Co-authored-by: NNeonael <neonaelangcayanazareno@gmail.com>

--- src/main/java/org/rocs/asa/service/profile/counselor/ProfileService.java
+++ src/main/java/org/rocs/asa/service/profile/counselor/ProfileService.java
@@ -3,8 +3,9 @@ package org.rocs.asa.service.profile.counselor;
 import org.rocs.asa.domain.account.profile.request.CounselorProfileDto;
 import org.rocs.asa.domain.guidance.staff.GuidanceStaff;
 import org.rocs.asa.domain.guidance.staff.request.profile.UpdateGuidanceStaffProfileRequest;
+import org.rocs.asa.domain.person.Person;
 
 public interface ProfileService {
     CounselorProfileDto getCounselorProfile (Long employeeNumber);
-    GuidanceStaff updateProfile (Long guidanceStaffId ,UpdateGuidanceStaffProfileRequest request);
+    Person updateProfile (String userId , UpdateGuidanceStaffProfileRequest request);
 }
