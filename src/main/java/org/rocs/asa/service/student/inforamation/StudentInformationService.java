323aa8d7bf0ac196a13cbb6e621906a8077d521d
NNeonael <neonaelangcayanazareno@gmail.com>
2026-02-15 14:33:00 +0800

    implement retriving all organization for dropdown (admin-page)

--- src/main/java/org/rocs/asa/service/student/inforamation/StudentInformationService.java
+++ src/main/java/org/rocs/asa/service/student/inforamation/StudentInformationService.java
@@ -1,8 +1,10 @@
 package org.rocs.asa.service.student.inforamation;
 
+import org.rocs.asa.domain.section.Section;
 import org.rocs.asa.domain.student.information.response.StudentDetailsResponse;
 import java.util.List;
 
 public interface StudentInformationService {
     List<StudentDetailsResponse> getAllStudent();
+    List<String> getAllOrganization();
 }
